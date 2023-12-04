e74766b6f2b7e65bd9bd3ec4224d73d3037c96e3
Peter D. Faria <zshift@gmail.com>
2023-12-04 18:26:22 -0500

    Extract commands from main

---
+++ src/command.rs
@@ -0,0 +1,185 @@
+use futures::future;
+use poise::{command, serenity_prelude as serenity};
+
+use crate::{db, discord, Context, Error, Result};
+
+// Adds experience to a player
+#[command(slash_command)]
+pub async fn exp(
+    ctx: Context<'_>,
+    #[description = "Player"] player: serenity::Member,
+    #[description = "Experience"] experience: u32,
+) -> Result<()> {
+    let conn = ctx.data().pool.clone().get()?;
+
+    let player_id = *player.user.id.as_u64() as i64;
+    let curr_xp = db::get_xp(&conn, player_id)?;
+    let new_xp = curr_xp + experience as i64;
+
+    db::set_xp(&conn, player_id, new_xp)?;
+
+    let response = format!(
+        "Updated {}'s account from {}xp to {}xp.",
+        player.user.name, curr_xp, new_xp
+    );
+    ctx.say(response).await?;
+    Ok(())
+}
+
+// Returns the experience of all players.
+#[command(slash_command)]
+pub async fn experience(ctx: Context<'_>) -> Result<()> {
+    let conn = ctx.data().pool.clone().get()?;
+
+    let id_xp = db::get_all_xp(&conn)?;
+    let user_xp_futures = id_xp
+        .iter()
+        .map(|(id, xp)| async move {
+            let user = discord::get_user(ctx, id).await?;
+            let nick = discord::get_nick_or_name(ctx, user).await;
+            Ok::<_, Error>(format!("{}: {}", nick, xp))
+        })
+        .collect::<Vec<_>>();
+
+    let user_xp = future::try_join_all(user_xp_futures).await?.join("\n");
+
+    ctx.say(user_xp).await?;
+    Ok(())
+}
+
+// Nominates a player as the MVP
+#[command(slash_command)]
+pub async fn mvp(ctx: Context<'_>, #[description = "MVP"] mvp: serenity::Member) -> Result<()> {
+    let conn = ctx.data().pool.clone().get()?;
+
+    let player_id = ctx.author().id.0 as i64;
+    let mvp_id = mvp.user.id.0 as i64;
+
+    let result = db::vote_for_mvp(&conn, player_id, mvp_id);
+    match result {
+        Ok(_) => {
+            let nick = discord::get_nick_or_name(ctx, mvp.user).await;
+            ctx.say(format!("Your vote for {} was registered", nick))
+                .await?;
+        }
+
+        Err(e) => {
+            ctx.say(format!("Error voting for MVP: {}", e)).await?;
+            return Ok(());
+        }
+    }
+    Ok(())
+}
+
+// Registers a player
+#[command(slash_command, rename = "registerplayer")]
+pub async fn register_player(
+    ctx: Context<'_>,
+    #[description = "Player"] player: serenity::Member,
+) -> Result<()> {
+    let conn = ctx.data().pool.clone().get()?;
+    let player_id = *player.user.id.as_u64() as i64;
+
+    db::create_player(&conn, player_id)?;
+    ctx.say(format!("Created {} with 0 experience.", player.user.name))
+        .await?;
+    Ok(())
+}
+
+// Resolves the MVP
+#[command(slash_command, rename = "resolve-mvp")]
+pub async fn resolve_mvp(ctx: Context<'_>) -> Result<()> {
+    let mut conn = ctx.data().pool.clone().get()?;
+
+    match db::resolve_mvp(&mut conn) {
+        Ok(mvp_id) => {
+            let mvp = discord::get_user(ctx, &mvp_id).await?;
+            let nick = discord::get_nick_or_name(ctx, mvp).await;
+
+            ctx.say(format!("The MVP is {}!", nick)).await?;
+        }
+
+        Err(e) => match e {
+            db::Error::MissingVotes => {
+                ctx.say("Not everyone has voted").await?;
+            }
+            db::Error::Chrono(e) => {
+                ctx.say(format!("Error parsing datetime: {}", e)).await?;
+            }
+            db::Error::Sqlite(e) => {
+                ctx.say(format!("Error querying database: {}", e)).await?;
+            }
+        },
+    }
+
+    Ok(())
+}
+
+// Rolls dice
+#[command(slash_command)]
+pub async fn roll(ctx: Context<'_>, #[description = "Dice"] dice: String) -> Result<()> {
+    let mut rng = ctx.data().rng.clone();
+
+    match evaluroll::eval(&mut rng, &dice).map_err(|e| e.to_string()) {
+        Ok(results) => {
+            ctx.say(format!(
+                "Rolled **{}** = {}",
+                dice,
+                discord::Output(&results)
+            ))
+            .await?;
+        }
+
+        Err(e) => {
+            ctx.say(format!("Error: {}", e)).await?;
+        }
+    }
+    Ok(())
+}
+
+// Schedules a game
+#[command(slash_command)]
+pub async fn schedule(
+    ctx: Context<'_>,
+    #[description = "Channel"] channel: serenity::Channel,
+    #[description = "Message"] msg: String,
+    #[description = "On"] on: serenity::Timestamp,
+) -> Result<()> {
+    log::info!("Scheduling message: {} on {}", msg, on);
+
+    let channel_id = *channel.id().as_u64();
+
+    let sch = db::ScheduledMessage {
+        channel_id,
+        msg,
+        on: (*on).into(),
+    };
+
+    {
+        let mut scheduler = ctx
+            .data()
+            .scheduler
+            .write()
+            .expect("Unable to get mut scheduler");
+
+        log::info!("Scheduling message");
+        scheduler.schedule(&sch)?;
+        log::info!("Scheduled message");
+    }
+
+    ctx.say("Message scheduled!").await?;
+
+    Ok(())
+}
+
+#[command(slash_command)]
+pub async fn connections(ctx: Context<'_>) -> Result<()> {
+    let pool = ctx.data().pool.clone();
+    ctx.say(format!(
+        "Connections: {}, Idle connections: {}",
+        pool.state().connections,
+        pool.state().idle_connections
+    ))
+    .await?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
+mod command;
 mod db;
 mod discord;
 mod scheduler;
 
 use dotenvy::dotenv;
-use futures::future;
 use poise::{
-    command,
     serenity_prelude::{self as serenity, GuildId},
     FrameworkError,
 };
@@ -18,7 +17,9 @@ use std::{
     sync::{Arc, RwLock},
 };
 
-use crate::db::ScheduledMessage;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data<serenity::Context, Hc128Rng>, Error>;
+type Result<T> = core::result::Result<T, Error>;
 
 // User data, which is stored and accessible in all command invocations
 struct Data<T, R>
@@ -31,11 +32,7 @@ where
     rng: R,
 }
 
-type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data<serenity::Context, Hc128Rng>, Error>;
-type Result<T> = core::result::Result<T, Error>;
-
-async fn handle_error(error: FrameworkError<'_, Data<serenity::Context, Hc128Rng>, Error>) {
+async fn handle_error<T>(error: FrameworkError<'_, T, Error>) {
     log::error!("Error: {}", error);
 
     if let Some(ctx) = error.ctx() {
@@ -62,14 +59,14 @@ async fn main() {
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
             commands: vec![
-                exp(),
-                experience(),
-                mvp(),
-                register_player(),
-                resolve_mvp(),
-                roll(),
-                schedule(),
-                connections(),
+                command::exp(),
+                command::experience(),
+                command::mvp(),
+                command::register_player(),
+                command::resolve_mvp(),
+                command::roll(),
+                command::schedule(),
+                command::connections(),
             ],
             on_error: |error| Box::pin(handle_error(error)),
             ..Default::default()
@@ -107,185 +104,4 @@ async fn main() {
 
     log::info!("Connecting to Discord...");
     framework.run().await.expect("Failed to start framework");
-}
-
-// Adds experience to a player
-#[command(slash_command)]
-async fn exp(
-    ctx: Context<'_>,
-    #[description = "Player"] player: serenity::Member,
-    #[description = "Experience"] experience: u32,
-) -> Result<()> {
-    let conn = ctx.data().pool.clone().get()?;
-
-    let player_id = *player.user.id.as_u64() as i64;
-    let curr_xp = db::get_xp(&conn, player_id)?;
-    let new_xp = curr_xp + experience as i64;
-
-    db::set_xp(&conn, player_id, new_xp)?;
-
-    let response = format!(
-        "Updated {}'s account from {}xp to {}xp.",
-        player.user.name, curr_xp, new_xp
-    );
-    ctx.say(response).await?;
-    Ok(())
-}
-
-// Returns the experience of all players.
-#[command(slash_command)]
-async fn experience(ctx: Context<'_>) -> Result<()> {
-    let conn = ctx.data().pool.clone().get()?;
-
-    let id_xp = db::get_all_xp(&conn)?;
-    let user_xp_futures = id_xp
-        .iter()
-        .map(|(id, xp)| async move {
-            let user = discord::get_user(ctx, id).await?;
-            let nick = discord::get_nick_or_name(ctx, user).await;
-            Ok::<_, Error>(format!("{}: {}", nick, xp))
-        })
-        .collect::<Vec<_>>();
-
-    let user_xp = future::try_join_all(user_xp_futures).await?.join("\n");
-
-    ctx.say(user_xp).await?;
-    Ok(())
-}
-
-// Nominates a player as the MVP
-#[command(slash_command)]
-async fn mvp(ctx: Context<'_>, #[description = "MVP"] mvp: serenity::Member) -> Result<()> {
-    let conn = ctx.data().pool.clone().get()?;
-
-    let player_id = ctx.author().id.0 as i64;
-    let mvp_id = mvp.user.id.0 as i64;
-
-    let result = db::vote_for_mvp(&conn, player_id, mvp_id);
-    match result {
-        Ok(_) => {
-            let nick = discord::get_nick_or_name(ctx, mvp.user).await;
-            ctx.say(format!("Your vote for {} was registered", nick))
-                .await?;
-        }
-
-        Err(e) => {
-            ctx.say(format!("Error voting for MVP: {}", e)).await?;
-            return Ok(());
-        }
-    }
-    Ok(())
-}
-
-// Registers a player
-#[command(slash_command, rename = "registerplayer")]
-async fn register_player(
-    ctx: Context<'_>,
-    #[description = "Player"] player: serenity::Member,
-) -> Result<()> {
-    let conn = ctx.data().pool.clone().get()?;
-    let player_id = *player.user.id.as_u64() as i64;
-
-    db::create_player(&conn, player_id)?;
-    ctx.say(format!("Created {} with 0 experience.", player.user.name))
-        .await?;
-    Ok(())
-}
-
-// Resolves the MVP
-#[command(slash_command, rename = "resolve-mvp")]
-async fn resolve_mvp(ctx: Context<'_>) -> Result<()> {
-    let mut conn = ctx.data().pool.clone().get()?;
-
-    match db::resolve_mvp(&mut conn) {
-        Ok(mvp_id) => {
-            let mvp = discord::get_user(ctx, &mvp_id).await?;
-            let nick = discord::get_nick_or_name(ctx, mvp).await;
-
-            ctx.say(format!("The MVP is {}!", nick)).await?;
-        }
-
-        Err(e) => match e {
-            db::Error::MissingVotes => {
-                ctx.say("Not everyone has voted").await?;
-            }
-            db::Error::Chrono(e) => {
-                ctx.say(format!("Error parsing datetime: {}", e)).await?;
-            }
-            db::Error::Sqlite(e) => {
-                ctx.say(format!("Error querying database: {}", e)).await?;
-            }
-        },
-    }
-
-    Ok(())
-}
-
-// Rolls dice
-#[command(slash_command)]
-async fn roll(ctx: Context<'_>, #[description = "Dice"] dice: String) -> Result<()> {
-    let mut rng = ctx.data().rng.clone();
-
-    match evaluroll::eval(&mut rng, &dice).map_err(|e| e.to_string()) {
-        Ok(results) => {
-            ctx.say(format!(
-                "Rolled **{}** = {}",
-                dice,
-                discord::Output(&results)
-            ))
-            .await?;
-        }
-
-        Err(e) => {
-            ctx.say(format!("Error: {}", e)).await?;
-        }
-    }
-    Ok(())
-}
-
-// Schedules a game
-#[command(slash_command)]
-async fn schedule(
-    ctx: Context<'_>,
-    #[description = "Channel"] channel: serenity::Channel,
-    #[description = "Message"] msg: String,
-    #[description = "On"] on: serenity::Timestamp,
-) -> Result<()> {
-    log::info!("Scheduling message: {} on {}", msg, on);
-
-    let channel_id = *channel.id().as_u64();
-
-    let sch = ScheduledMessage {
-        channel_id,
-        msg,
-        on: (*on).into(),
-    };
-
-    {
-        let mut scheduler = ctx
-            .data()
-            .scheduler
-            .write()
-            .expect("Unable to get mut scheduler");
-
-        log::info!("Scheduling message");
-        scheduler.schedule(&sch)?;
-        log::info!("Scheduled message");
-    }
-
-    ctx.say("Message scheduled!").await?;
-
-    Ok(())
-}
-
-#[command(slash_command)]
-async fn connections(ctx: Context<'_>) -> Result<()> {
-    let pool = ctx.data().pool.clone();
-    ctx.say(format!(
-        "Connections: {}, Idle connections: {}",
-        pool.state().connections,
-        pool.state().idle_connections
-    ))
-    .await?;
-    Ok(())
 }
